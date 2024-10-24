e0e2e3af902bda4d082f0638c55b36e1d04a1edc
kp059 <kumarpatil059@gmail.com>
2024-10-24 18:57:37 +0530

    save data and generate html for payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import {cart, removeFromCart,updatDeliveryOption} from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products ,getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import  dayjs  from "https://unpkg.com/dayjs@1.11.10/esm/index.js"; // default export each file can only have 1 default expert
-import {deliveryOptions} from "../../data/deliveryOptions.js"
+import {deliveryOptions,getDeliveryOption} from "../../data/deliveryOptions.js";
 
 hello();
 const today=dayjs();
@@ -16,20 +16,12 @@ export function renderOrderSummary(){
     cart.forEach((cartItem)=>{
 
         const productId=cartItem.productId;
-        let matchingProduct;
-        products.forEach((product)=>{
-            if(product.id===productId){
-                matchingProduct=product;
-            }
-        });
 
-        const deliveryOptionId=cartItem.deliveryOptionId;
-        let deliveryOption;
-        deliveryOptions.forEach((option)=>{
-            if (option.id===deliveryOptionId) {
-                deliveryOption=option;
-            }
-        });
+        const matchingProduct=getProduct(productId);
+
+        const deliveryOptionId=cartItem.deliveryOptionId
+
+        const deliveryOption=getDeliveryOption(deliveryOptionId);
 
         const today=dayjs();
         const deliveryDate=today.add(
