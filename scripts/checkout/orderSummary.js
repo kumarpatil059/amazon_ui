75556f00b7cf7dfd45b77b9ee48df51f8b7fc0e7
kp059 <kumarpatil059@gmail.com>
2025-01-13 11:47:15 +0530

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,11 +4,7 @@ import { formatCurrency } from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import  dayjs  from "https://unpkg.com/dayjs@1.11.10/esm/index.js"; // default export each file can only have 1 default expert
 import {deliveryOptions,getDeliveryOption} from "../../data/deliveryOptions.js";
-
-hello();
-const today=dayjs();
-const deliveryDate=today.add('7','days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
     let cartSummaryHTML='';
@@ -123,6 +119,7 @@ export function renderOrderSummary(){
             const container=document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
             
+            renderPaymentSummary();
             
         });
     });
@@ -132,6 +129,7 @@ export function renderOrderSummary(){
         const {productId,deliveryOptionId}=element.dataset; //shorthand property 
         updatDeliveryOption(productId,deliveryOptionId)
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
